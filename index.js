038944bfc75cb699ce63891f52b45c17efbd94cf
Cierra Ray <cierraray2@gmail.com>
2022-09-19 19:36:48 -0400

    Assignment 2 commit

---
+++ index.js
@@ -0,0 +1,40 @@
+//#1
+let myNumber;
+//#2
+    myNumber = 0;
+    myNumber = 5;
+//#3
+let myOtherNumber 
+    myOtherNumber = 0;
+    myOtherNumber = 5;
+//#4
+let myTotal 
+    myTotal = 0;
+    myTotal = myNumber* myOtherNumber;
+//5
+let myName;
+    myName = '';
+let myCombo; 
+    myCombo= '';
+    myCombo = toString(myNumber * myOtherNumber);
+//#6
+let myOtherCombo;
+myOtherCombo = 0;
+myOtherCombo = myNumber + myOtherNumber; 
+//#7
+let myRemainder;
+    myRemainder = 0;
+    myRemainder = myNumber/5;
+//#8
+let i;
+i = 0;
+i = 2;
+
+//#9
+let int;
+int = 0;
+int = 4;
+//#10
+let myBoolean;
+    myBoolean = true;
+    myBoolean = false;
